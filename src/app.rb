2be57eed9422083e6f2edbf06ca7cd5d3d9d450e
hugoarmor <hugoarmoreira@hotmail.com>
2024-01-30 12:29:57 -0300

    add: /:short_code get handler

--- src/app.rb
+++ src/app.rb
@@ -10,11 +10,21 @@ url_shortener = UrlShortenerService.new
 post '/shorten' do
   content_type :json
 
-  data = JSON.parse(request.body.read) if request.body.read.present?
+  params = JSON.parse request.body.read
 
-  raise "Please, provide the required params." unless data&["url"].present? && data&["short_code"].present?
+  raise "Please, provide the required params." unless params["url"].present? && params["short_code"].present?
 
-  result = url_shortener.shorten(url: data["url"], short_code: data["short_code"])
+  result = url_shortener.shorten(url: params["url"], short_code: params["short_code"])
 
   result.to_json
+end
+
+get '/:short_code' do
+  content_type :json
+
+  result = url_shortener.consume(params[:short_code])
+
+  headers['Location'] = result
+
+  halt 304
 end
